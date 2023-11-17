d805e189e67e9c2bca1a6c3d82cfc55dc12d3351
Oleksandr-Heleta <sanock.geleta@gmail.com>
2023-11-17 16:08:20 +0200

    added products

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { CellAction } from "./cell-action";
+
+export type ProductColumn = {
+  id: string;
+  name: string;
+  price: string;
+  category: string;
+  size: string;
+  collor: string;
+  isFeatured: boolean;
+  isArchived: boolean;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Archived",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+  },
+  {
+    accessorKey: "collor",
+    header: "Collor",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        {row.original.collor}
+        <div
+          className="w-6 h-6 rounded-full"
+          style={{ backgroundColor: row.original.collor }}
+        ></div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "action",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
