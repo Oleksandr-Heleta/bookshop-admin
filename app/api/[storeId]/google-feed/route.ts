4beb3acc76f4740ebc72a5a70a1a34c0bc402616
Oleksandr-Heleta <sanock.geleta@gmail.com>
2024-09-17 23:18:53 +0300

    fixed google feed, added webp converter

--- app/api/[storeId]/google-feed/route.ts
+++ app/api/[storeId]/google-feed/route.ts
@@ -32,7 +32,11 @@ export async function GET(
       categories: true,
       ageGroups: true,
       publishing: true,
-      images: true,
+      images: {
+        orderBy: {
+          order: 'asc',
+        },
+      },
     },
   });
 
@@ -49,67 +53,65 @@ export async function GET(
   });
 
   const xml = `
-    <?xml version="1.0" encoding="UTF-8"?>
-    <rss xmlns:g="${googleShoppingNamespace}" version="2.0">
-      <channel>
-        <title>Магазин дитячих книг МИШКА</title>
-        <link>${process.env.FRONTEND_STORE_URL}</link>
-        <description>Купуйте книги для дітей онлайн</description>
-        ${products
+<?xml version="1.0" encoding="UTF-8"?>
+<rss xmlns:g="${googleShoppingNamespace}" version="2.0">
+  <channel>
+    <title>Магазин дитячих книг МИШКА</title>
+    <link>${process.env.FRONTEND_STORE_URL}</link>
+    <description>Купуйте книги для дітей онлайн</description>
+    ${products
+      .map(
+        (product) => `
+      <item>
+        <g:id>${product.id}</g:id>
+        <g:title><![CDATA[${product.name}]]></g:title>
+        <g:description><![CDATA[${product.description}]]></g:description>
+        <g:link>${process.env.FRONTEND_STORE_URL}/product/${product.id}</g:link>
+        <g:image_link>${product.images[0]?.url ?? ''}</g:image_link>
+        <g:price>${product.price} UAH</g:price>
+        <g:availability>${
+          product.quantity > 0 ? 'in stock' : 'out of stock'
+        }</g:availability>
+        <g:brand><![CDATA[${product.publishing.name}]]></g:brand>
+        <g:google_product_category>![CDATA[Media > Books > Children's Books]]</g:google_product_category>
+        <g:product_type><![CDATA[Books & Magazines > Books > Children's Books]]></g:product_type>
+        <g:condition>new</g:condition>
+        <g:sale_price>${
+          product.isSale
+            ? product.price.toNumber() -
+              (product.price.toNumber() * product.sale) / 100
+            : product.price
+        } UAH</g:sale_price>
+        <g:item_group_id>${product.id}</g:item_group_id>
+        <g:age_group>kids</g:age_group>
+        <g:custom_label_0>![CDATA[Вік: ${product.ageGroups
           .map(
-            (product) => `
-          <item>
-            <g:id>${product.id}</g:id>
-            <g:title><![CDATA[${product.name}]]></g:title>
-            <g:description><![CDATA[${product.description}]]></g:description>
-            <g:link>${process.env.FRONTEND_STORE_URL}/product/${
-              product.id
-            }</g:link>
-            <g:image_link>${product.images[0]?.url ?? ''}</g:image_link>
-            <g:price>${product.price} UAH</g:price>
-            <g:availability>${
-              product.quantity > 0 ? 'in stock' : 'out of stock'
-            }</g:availability>
-            <g:brand><![CDATA[${product.publishing.name}]]></g:brand>
-            <g:google_product_category>Media > Books > Children's Books</g:google_product_category>
-            <g:product_type><![CDATA[Books & Magazines > Books > Children's Books]]></g:product_type>
-            <g:condition>new</g:condition>
-            <g:sale_price>${
-              product.isSale
-                ? product.price.toNumber() -
-                  (product.price.toNumber() * product.sale) / 100
-                : product.price
-            } UAH</g:sale_price>
-            <g:item_group_id>${product.id}</g:item_group_id>
-            <g:age_group>kids</g:age_group>
-            <g:custom_label_0>Вік: ${product.ageGroups
-              .map(
-                (age) =>
-                  ageGroups.find((ageGroup) => ageGroup.id === age.ageGroupId)
-                    ?.name
-              )
-              .join(', ')} роки</g:custom_label_0>
-               <g:custom_label_1>Категорія: ${product.categories
-                 .map(
-                   (category) =>
-                     categories.find((cat) => cat.id === category.categoryId)
-                       ?.name
-                 )
-                 .join(', ')}</g:custom_label_1>
-              <g:shipping>
-  <g:country>UA</g:country>
-  <g:service>Standard Shipping</g:service>
-  
-</g:shipping>
-          </item>
-        `
+            (age) =>
+              ageGroups.find((ageGroup) => ageGroup.id === age.ageGroupId)?.name
           )
-          .join('')}
-      </channel>
-    </rss>
+          .join(', ')} роки]]</g:custom_label_0>
+        <g:custom_label_1>![CDATA[Категорія: ${product.categories
+          .map(
+            (category) =>
+              categories.find((cat) => cat.id === category.categoryId)?.name
+          )
+          .join(', ')}]]</g:custom_label_1>
+        <g:shipping>
+          <g:country>UA</g:country>
+          <g:service>Standard Shipping</g:service>
+        </g:shipping>
+      </item>
+    `
+      )
+      .join('')}
+  </channel>
+</rss>
   `;
 
-  const response = new NextResponse(xml, { status: 200, headers: corsHeaders });
+  const response = new NextResponse(xml.trim(), {
+    status: 200,
+    headers: corsHeaders,
+  });
   response.headers.set('Content-Type', 'application/xml');
   return response;
 }
--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,14 +1,13 @@
-import { writeFile } from 'fs/promises'
-import path from 'path'
-import { NextRequest, NextResponse } from 'next/server'
+import { writeFile } from 'fs/promises';
+import path from 'path';
+import { NextRequest, NextResponse } from 'next/server';
 import fs from 'fs';
 import sharp from 'sharp';
 
 export async function POST(request: NextRequest) {
   const data = await request.formData();
   const files: File[] = [];
-  
-  
+
   let index = 0;
   while (true) {
     const file = data.get(`Files[${index}]`) as File | null;
@@ -16,24 +15,32 @@ export async function POST(request: NextRequest) {
     files.push(file);
     index++;
   }
-  
+
   // console.log("Retrieved files:", files);
 
   if (!files || files.length === 0) {
     return NextResponse.json({ success: false });
   }
 
-  const filePaths = await Promise.all(files.map(async (file) => {
-    const bytes = await file.arrayBuffer();
-    const buffer = await sharp(bytes).toBuffer();
-    const fileName = `${Date.now()}_${file.name}`;
-    const filePath = path.join(process.cwd(), 'images', fileName); // change public to images folder
-    await writeFile(filePath, buffer);
-    return {
-      filePath,
-      fileName,
-    };
-  }));
+  const filePaths = await Promise.all(
+    files.map(async (file) => {
+      const bytes = await file.arrayBuffer();
+      const buffer = Buffer.from(bytes);
+      const webpBuffer = await sharp(buffer)
+        .webp({ quality: 60 }) // Конвертуємо в формат WebP з якістю 90
+        .toBuffer();
+      const fileName = `${Date.now()}_${file.name
+        .split('.')
+        .slice(0, -1)
+        .join('.')}.webp`;
+      const filePath = path.join(process.cwd(), 'images', fileName); // change public to images folder
+      await writeFile(filePath, webpBuffer);
+      return {
+        filePath,
+        fileName,
+      };
+    })
+  );
 
   return NextResponse.json({ success: true, paths: filePaths });
 }
@@ -42,25 +49,40 @@ export async function DELETE(request: NextRequest) {
   const requestBody = await request.json();
 
   if (!requestBody) {
-    return NextResponse.json({ success: false, message: 'Request body is required' });
+    return NextResponse.json({
+      success: false,
+      message: 'Request body is required',
+    });
   }
 
-  
   const { filename } = requestBody;
 
   if (!filename) {
-    return NextResponse.json({ success: false, message: 'Filename is required' });
+    return NextResponse.json({
+      success: false,
+      message: 'Filename is required',
+    });
   }
 
-  const filePath = path.join(process.cwd(), 'images', filename.replace(process.env.NEXT_PUBLIC_IMAGE_STORE_URL, ''));
+  const filePath = path.join(
+    process.cwd(),
+    'images',
+    filename.replace(process.env.NEXT_PUBLIC_IMAGE_STORE_URL, '')
+  );
 
   try {
-    await fs.promises.access(filePath)
-    await fs.promises.unlink(filePath)
+    await fs.promises.access(filePath);
+    await fs.promises.unlink(filePath);
 
-    return NextResponse.json({ success: true, message: 'File deleted successfully' })
+    return NextResponse.json({
+      success: true,
+      message: 'File deleted successfully',
+    });
   } catch (error) {
-    console.error(`Error deleting file: ${filePath}`, error)
-    return NextResponse.json({ success: false, message: 'Error deleting file' })
+    console.error(`Error deleting file: ${filePath}`, error);
+    return NextResponse.json({
+      success: false,
+      message: 'Error deleting file',
+    });
   }
 }
